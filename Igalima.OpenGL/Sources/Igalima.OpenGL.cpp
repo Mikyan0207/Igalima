ff0b9e63e1aa81d8b9ce7522c66db397c3c3a05c
Florian Theron <florian.theron@epitech.eu>
2021-02-09 16:33:23 +0100

    Update to OpenGL 4.6

--- Igalima.OpenGL/Sources/Igalima.OpenGL.cpp
+++ Igalima.OpenGL/Sources/Igalima.OpenGL.cpp
@@ -125,8 +125,8 @@ int main()
         std::abort();
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
