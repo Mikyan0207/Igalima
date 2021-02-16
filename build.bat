df3a46d31a2ae4436d0efc92cf00f9bccc84cf30
Florian Theron <florian.theron@epitech.eu>
2021-02-16 13:15:30 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@
 
 set files= ..\Sources\main.cpp ..\Sources\Platform\Window.cpp ..\Sources\Renderer\Renderer.cpp ^
 		   ..\Sources\glad.c
-set options= /Zi /EHsc /W3 /Fe"Igalima.OpenGL.exe"
+set options= /Zi /EHsc /W3 /std:c++17 /MDd /Fe"Igalima.OpenGL.exe"
 
 pushd build
 :: Change this line if you want to compile on your PC.
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl %options% /I..\Includes\ /I..\Sources\ %files% /link /NODEFAULTLIB:..\Lib\glfw3.lib
+cl %options% /I"D:\GitHub\Igalima.OpenGL\Includes" /I"..\Sources" %files% /DYNAMICBASE "glfw3.lib" "user32.lib" "gdi32.lib" "shell32.lib" /link /LIBPATH:..\Lib\
 popd
