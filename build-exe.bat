faa1bf5b16f1a1e68485cc4b564635fe80a3f3a3
Florian Theron <florian.theron@epitech.eu>
2021-02-19 21:59:30 +0100

    Add option to build an exe for debug&dev

--- build-exe.bat
+++ build-exe.bat
@@ -1,11 +1,10 @@
 @echo off
 
-:: Visual Studio CL compiler path.
-:: Change this line if you want to compile on your PC.
 set CLCompilerPath= "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 
+
 :: Source files.
-set SourceFiles=  ..\Sources\OS\Win32\Win32Window.cpp ..\Sources\Memory\StackAllocator.cpp
+set SourceFiles= ..\Sources\main.cpp ..\Sources\OS\Win32\Win32Window.cpp ..\Sources\Memory\StackAllocator.cpp
 
 :: Inclues files directory.
 set DebugIncludeFiles= /I"..\Includes" /I"..\Sources"
@@ -14,12 +13,14 @@ set DebugIncludeFiles= /I"..\Includes" /I"..\Sources"
 set DebugLibraries= "opengl32.lib" "user32.lib" "gdi32.lib" "winmm.lib"
 
 :: Debug Compiler Flags.
-set DebugCompilerOptions= /DDEBUG /D_USRDLL /D_WINDLL /MDd /LD /INCREMENTAL:no /nologo /GR /Od /Oi /WX /W4 /Zi /EHsc /std:c++17 /Fe"Igalima" /Fm"Igalima.map"
+set DebugCompilerOptions= /DDEBUG /MDd /nologo /GR /Od /Oi /WX /W4 /Zi /EHsc /std:c++17 /Fe"Igalima" /Fm"Igalima.map"
 
 :: Linker Flags.
-set DebugLinkerOptions= /link /DLL
+set DebugLinkerOptions=
 
-pushd build
+IF NOT EXIST build-exe mkdir build-exe
+
+pushd build-exe
 call %CLCompilerPath% x64
 cl %DebugCompilerOptions% %DebugIncludeFiles% %SourceFiles% %DebugLibraries% %DebugLinkerOptions%
 popd
