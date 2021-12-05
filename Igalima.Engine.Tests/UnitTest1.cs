779e06fcc045cc6876e0f13e7dc960e148feaad8
Florian Theron <florian.theron@epitech.eu>
2021-12-05 14:58:51 +0100

    Add Unit Tests projects

---
+++ Igalima.Engine.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Igalima.Engine.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
